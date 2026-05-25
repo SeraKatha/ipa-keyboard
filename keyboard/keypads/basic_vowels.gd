cc769ceb02a220bb22d7ced386e69f2e4577917b
Sera Katharina Litsch <sera.katharina.litsch@gmail.com>
2026-05-26 00:27:15 +0200

    Added key bindings for vowels

--- keyboard/keypads/basic_vowels.gd
+++ keyboard/keypads/basic_vowels.gd
@@ -3,46 +3,110 @@ extends BasicKeyPad
 func _ready() -> void:
 	var vowels = [
 		[
-			[sound("i"), sound("y")],
-			[sound("ɪ"), sound("ʏ")],
-			[sound("ɨ"), sound("ʉ")],
-			[sound("ʊ"), null], 
-			[sound("ɯ"), sound("u")],
+			[
+				sound("i").overide_name("vowel_close_front_unrounded"),
+				sound("y").overide_name("vowel_close_front_rounded"),
+			],
+			[null, null],
+			[
+				sound("ɨ").overide_name("vowel_close_central_unrounded"),
+				sound("ʉ").overide_name("vowel_close_central_rounded")
+			],
+			[null, null],
+			[
+				sound("ɯ").overide_name("vowel_close_back_unrounded"),
+				sound("u").overide_name("vowel_close_back_rounded")
+			],
 		], 
 		[
-			[sound("e"), sound("ø")],
 			[null, null],
-			[sound("ɘ"), sound("ɵ")],
+			[
+				sound("ɪ").overide_name("vowel_nearclose_nearfront_unrounded"),
+				sound("ʏ").overide_name("vowel_nearclose_nearfront_rounded")
+			],
 			[null, null],
-			[sound("ɤ"),
-			sound("o")]],
+			[
+				sound("ʊ").overide_name("vowel_nearclose_nearback"),
+				null
+			],
+			[null, null],
+		], 
 		[
-			[sound("e̞"), sound("ø̞")],
+			[
+				sound("e").overide_name("vowel_closemid_front_unrounded"),
+				sound("ø").overide_name("vowel_closemid_front_rounded"),
+			],
 			[null, null],
-			[sound("ə"), null],
+			[
+				sound("ɘ").overide_name("vowel_closemid_central_unrounded"),
+				sound("ɵ").overide_name("vowel_closemid_central_rounded"),
+			],
 			[null, null],
-			[sound("o̞"), sound("ɤ̞")],
+			[
+				sound("ɤ").overide_name("vowel_closemid_back_unrounded"),
+				sound("o").overide_name("vowel_closemid_back_rounded")
+			]
 		],
 		[
-			[sound("ɛ"), sound("œ")],
+			[
+				sound("e̞").overide_name("vowel_mid_front_unrounded"),
+				sound("ø̞").overide_name("vowel_mid_front_rounded")
+			],
 			[null, null],
-			[sound("ɜ"), sound("ɞ")],
+			[
+				sound("ə").overide_name("vowel_mid_central"),
+				null,
+			],
 			[null, null],
-			[sound("ʌ"), sound("ɔ")],
+			[
+				sound("ɤ̞").overide_name("vowel_mid_back_unrounded"),
+				sound("o̞").overide_name("vowel_mid_back_rounded"),
+			],
 		],
 		[
-			[sound("æ"), null],
+			[
+				sound("ɛ").overide_name("vowel_openmid_front_unrounded"),
+				sound("œ").overide_name("vowel_openmid_front_rounded")
+			],
 			[null, null],
-			[sound("ɐ"), null],
+			[
+				sound("ɜ").overide_name("vowel_openmid_central_unrounded"),
+				sound("ɞ").overide_name("vowel_openmid_central_rounded")
+			],
+			[null, null],
+			[
+				sound("ʌ").overide_name("vowel_openmid_back_unrounded"),
+				sound("ɔ").overide_name("vowel_openmid_back_rounded"),
+			],
+		],
+		[
+			[
+				sound("æ").overide_name("vowel_nearopen_front_unrounded"),
+				null
+			],
+			[null, null],
+			[
+				sound("ɐ").overide_name("vowel_nearopen_central"),
+				null
+			],
 			[null, null],
 			[null, null],
 		],
 		[
-			[sound("a"), sound("ɶ")],
+			[
+				sound("a").overide_name("vowel_open_front_unrounded"),
+				sound("ɶ").overide_name("vowel_open_front_rounded"),
+			],
 			[null, null],
-			[sound("ä"), null],
+			[
+				sound("ä").overide_name("vowel_open_central"),
+				null
+			],
 			[null, null],
-			[sound("ɑ"), sound("ɒ")],
+			[
+				sound("ɑ").overide_name("vowel_open_back_unrounded"),
+				sound("ɒ").overide_name("vowel_open_back_rounded")
+			],
 		],
 	]
 	
