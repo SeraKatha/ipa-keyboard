6927f2f112444636714146dffe8338f0b49044db
Sera Katharina Litsch <sera.katharina.litsch@gmail.com>
2026-05-27 23:05:21 +0200

    Minor styling tweaks and condensed layout

--- keyboard/keys/basic_key.gd
+++ keyboard/keys/basic_key.gd
@@ -6,9 +6,9 @@ func input_event_with_modifiert_to_string(input_event : InputEventWithModifiers)
 	if input_event.shift_pressed:
 		hint = "⇧%s" % hint.to_upper()
 	if input_event.alt_pressed:
-		hint = "⌥%s" % hint
+		hint = "\\%s" % hint
 	if input_event.ctrl_pressed:
-		hint = "⌃%s" % hint
+		hint = "^%s" % hint
 	return hint
 
 func get_primary_event(input_action_name : StringName) -> InputEvent:
