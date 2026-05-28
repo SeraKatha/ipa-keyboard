b3d9c2a0ad3df6307648341d6ce3b62a70380d1b
Sera <sera.katharina.litsch@gmail.com>
2026-05-28 17:14:17 +0200

    Added basic locking to prevent multiple instances

--- main.gd
+++ main.gd
@@ -12,6 +12,10 @@ var _current_text : String = ""
 var _cursor_position = 0
 var _caret_stop : Array[int] = []
 
+
+const LOCK_EXPIRATION = 2;
+
+
 func _init_config():
 	if not FileAccess.file_exists("user://config.cfg"):
 		var config = ConfigFile.new()
@@ -30,31 +34,47 @@ func _load_config():
 	get_tree().root.content_scale_factor = application_scale
 	var window_size = Vector2i(Vector2(890, 344) * application_scale)
 	DisplayServer.window_set_size(window_size)
-	for key in config.get_section_keys("key_bindings"):
-		var event_action_str = config.get_value("key_bindings", key).to_lower()
-		var event_action_keys = event_action_str.split("+")
-		var input_event_key = InputMap.action_get_events(key)[0]
-		input_event_key.shift_pressed = event_action_keys.has("shift")
-		input_event_key.ctrl_pressed = event_action_keys.has("ctrl")
-		input_event_key.alt_pressed = event_action_keys.has("alt")
-		input_event_key.keycode = OS.find_keycode_from_string(event_action_keys[-1])
+	if config.has_section("key_bindings"):
+		for key in config.get_section_keys("key_bindings"):
+			var event_action_str = config.get_value("key_bindings", key).to_lower()
+			var event_action_keys = event_action_str.split("+")
+			var input_event_key = InputMap.action_get_events(key)[0]
+			input_event_key.shift_pressed = event_action_keys.has("shift")
+			input_event_key.ctrl_pressed = event_action_keys.has("ctrl")
+			input_event_key.alt_pressed = event_action_keys.has("alt")
+			input_event_key.keycode = OS.find_keycode_from_string(event_action_keys[-1])
 	var screen_size : Vector2i = DisplayServer.screen_get_size();
 	var window_position : Vector2i = screen_size / Vector2i(2, 1) - window_size / 2
 	DisplayServer.window_set_position(window_position)
 
+const FILE_LOCK : String = "user://lock"
+
+func _is_lock_fresh():
+	var lock_file = FileAccess.open(FILE_LOCK, FileAccess.READ)
+	var timestamp_lock = lock_file.get_64()
+	lock_file.close()
+	var timestamp_now = Time.get_unix_time_from_system()
+	var is_already_running = timestamp_now - timestamp_lock < 2
+	return is_already_running
+
 func _ready() -> void:
 	_init_config()
 	_load_config()
-	_keyboard = KEYBOARD.instantiate()
-	_container.add_child(_keyboard)
-	_keyboard.pressed_backspace.connect(_on_keyboard_pressed_backspace)
-	_keyboard.pressed_clear.connect(_on_keyboard_pressed_clear)
-	_keyboard.pressed_close.connect(_on_keyboard_pressed_close)
-	_keyboard.pressed_copy.connect(_on_keyboard_pressed_copy)
-	_keyboard.pressed_delete.connect(_on_keyboard_pressed_delete)
-	_keyboard.pressed_left.connect(_on_keyboard_pressed_left)
-	_keyboard.pressed_right.connect(_on_keyboard_pressed_right)
-	_keyboard.typed_sound.connect(_on_keyboard_typed_sound)
+	
+	if FileAccess.file_exists(FILE_LOCK) and _is_lock_fresh():
+		get_tree().quit()
+	else:
+		_refresh_lock()
+		_keyboard = KEYBOARD.instantiate()
+		_container.add_child(_keyboard)
+		_keyboard.pressed_backspace.connect(_on_keyboard_pressed_backspace)
+		_keyboard.pressed_clear.connect(_on_keyboard_pressed_clear)
+		_keyboard.pressed_close.connect(_on_keyboard_pressed_close)
+		_keyboard.pressed_copy.connect(_on_keyboard_pressed_copy)
+		_keyboard.pressed_delete.connect(_on_keyboard_pressed_delete)
+		_keyboard.pressed_left.connect(_on_keyboard_pressed_left)
+		_keyboard.pressed_right.connect(_on_keyboard_pressed_right)
+		_keyboard.typed_sound.connect(_on_keyboard_typed_sound)
 
 
 func _on_keyboard_typed_sound(sound: IPA.Sound) -> void:
@@ -125,3 +145,19 @@ func _on_keyboard_pressed_right() -> void:
 
 func _on_keyboard_pressed_close() -> void:
 	get_tree().quit()
+
+
+func _refresh_lock():
+	var	lock_file = FileAccess.open(FILE_LOCK, FileAccess.WRITE)
+	lock_file.store_64(Time.get_unix_time_from_system())
+	lock_file.close()
+	
+	
+func _exit_tree() -> void:
+	DirAccess.remove_absolute(FILE_LOCK)
+
+func _on_lock_timer_timeout() -> void:
+	_refresh_lock()
+
+func _on_tree_exiting() -> void:
+	print("AAA")
