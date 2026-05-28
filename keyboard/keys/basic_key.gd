53c5b951301769b2720346a829f20e2e94fbaf97
Sera Katharina Litsch <sera.katharina.litsch@gmail.com>
2026-05-28 06:26:27 +0200

    Improved some shortcut hints by replacing key names with key symbols

--- keyboard/keys/basic_key.gd
+++ keyboard/keys/basic_key.gd
@@ -2,13 +2,24 @@
 
 
 func input_event_with_modifiert_to_string(input_event : InputEventWithModifiers) -> String:
-	var hint := OS.get_keycode_string(input_event.keycode).to_lower()
+	var hint := keycode_to_string(input_event.keycode).to_lower()
 	if input_event.shift_pressed:
 		hint = "⇧%s" % hint.to_upper()
 	if input_event.alt_pressed:
 		hint = "\\%s" % hint
 	if input_event.ctrl_pressed:
 		hint = "^%s" % hint
+	return hint
+
+
+func keycode_to_string(keycode : int) -> String:
+	var hint := OS.get_keycode_string(keycode).to_lower()
+	if hint == "period":
+		hint = "."
+	if hint == "plus":
+		hint = "+"
+	if hint == "numbersign":
+		hint = "#"
 	return hint
 
 func get_primary_event(input_action_name : StringName) -> InputEvent:
