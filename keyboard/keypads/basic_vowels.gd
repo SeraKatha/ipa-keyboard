38b6f4260793179f1607094595a8de5dc2505d7b
Sera Katharina Litsch <sera.katharina.litsch@gmail.com>
2026-05-28 20:48:24 +0200

    Moved vowels into separate resource files

--- keyboard/keypads/basic_vowels.gd
+++ keyboard/keypads/basic_vowels.gd
@@ -4,89 +4,89 @@ func _ready() -> void:
 	var vowels = [
 		[
 			[
-				[sound("i"), "ipa_vowel_close_front_unrounded"],
-				[sound("y"), "ipa_vowel_close_front_rounded"],
+				IPA.VOWEL_CLOSE_FRONT_UNROUNDED,
+				IPA.VOWEL_CLOSE_FRONT_ROUNDED,
 			],
 			[null, null],
 			[
-				[sound("ɨ"), "ipa_vowel_close_central_unrounded"],
-				[sound("ʉ"), "ipa_vowel_close_central_rounded"],
+				IPA.VOWEL_CLOSE_CENTRAL_UNROUNDED,
+				IPA.VOWEL_CLOSE_CENTRAL_ROUNDED,
 			],
 			[null, null],
 			[
-				[sound("ɯ"), "ipa_vowel_close_back_unrounded"],
-				[sound("u"), "ipa_vowel_close_back_rounded"],
+				IPA.VOWEL_CLOSE_BACK_UNROUNDED,
+				IPA.VOWEL_CLOSE_BACK_ROUNDED,
 			],
 		], 
 		[
 			[null, null],
 			[
-				[sound("ɪ"), "ipa_vowel_nearclose_nearfront_unrounded"],
-				[sound("ʏ"), "ipa_vowel_nearclose_nearfront_rounded"],
+				IPA.VOWEL_NEARCLOSE_NEARFRONT_UNROUNDED,
+				IPA.VOWEL_NEARCLOSE_NEARFRONT_ROUNDED,
 			],
 			[null, null],
 			[
-				[sound("ʊ"), "ipa_vowel_nearclose_nearback"],
+				IPA.VOWEL_NEARCLOSE_NEARBACK,
 				null
 			],
 			[null, null],
 		], 
 		[
 			[
-				[sound("e"), "ipa_vowel_closemid_front_unrounded"],
-				[sound("ø"), "ipa_vowel_closemid_front_rounded"],
+				IPA.VOWEL_CLOSEMID_FRONT_UNROUNDED,
+				IPA.VOWEL_CLOSEMID_FRONT_ROUNDED,
 			],
 			[null, null],
 			[
-				[sound("ɘ"), "ipa_vowel_closemid_central_unrounded"],
-				[sound("ɵ"), "ipa_vowel_closemid_central_rounded"],
+				IPA.VOWEL_CLOSEMID_CENTRAL_UNROUNDED,
+				IPA.VOWEL_CLOSEMID_CENTRAL_ROUNDED,
 			],
 			[null, null],
 			[
-				[sound("ɤ"), "ipa_vowel_closemid_back_unrounded"],
-				[sound("o"), "ipa_vowel_closemid_back_rounded"],
+				IPA.VOWEL_CLOSEMID_BACK_UNROUNDED,
+				IPA.VOWEL_CLOSEMID_BACK_ROUNDED,
 			]
 		],
 		[
 			[
-				[sound("e̞"), "ipa_vowel_mid_front_unrounded"],
-				[sound("ø̞"), "ipa_vowel_mid_front_rounded"],
+				IPA.VOWEL_MID_FRONT_UNROUNDED,
+				IPA.VOWEL_MID_FRONT_ROUNDED,
 			],
 			[null, null],
 			[
-				[sound("ə"), "ipa_vowel_mid_central"],
+				IPA.VOWEL_MID_CENTRAL,
 				null,
 			],
 			[null, null],
 			[
-				[sound("ɤ̞"), "ipa_vowel_mid_back_unrounded"],
-				[sound("o̞"), "ipa_vowel_mid_back_rounded"],
+				IPA.VOWEL_MID_BACK_UNROUNDED,
+				IPA.VOWEL_MID_BACK_ROUNDED,
 			],
 		],
 		[
 			[
-				[sound("ɛ"), "ipa_vowel_openmid_front_unrounded"],
-				[sound("œ"), "ipa_vowel_openmid_front_rounded"],
+				IPA.VOWEL_OPENMID_FRONT_UNROUNDED,
+				IPA.VOWEL_OPENMID_FRONT_ROUNDED,
 			],
 			[null, null],
 			[
-				[sound("ɜ"), "ipa_vowel_openmid_central_unrounded"],
-				[sound("ɞ"), "ipa_vowel_openmid_central_rounded"]
+				IPA.VOWEL_OPENMID_CENTRAL_UNROUNDED,
+				IPA.VOWEL_OPENMID_CENTRAL_ROUNDED
 			],
 			[null, null],
 			[
-				[sound("ʌ"), "ipa_vowel_openmid_back_unrounded"],
-				[sound("ɔ"), "ipa_vowel_openmid_back_rounded"],
+				IPA.VOWEL_OPENMID_BACK_UNROUNDED,
+				IPA.VOWEL_OPENMID_BACK_ROUNDED,
 			],
 		],
 		[
 			[
-				[sound("æ"), "ipa_vowel_nearopen_front_unrounded"],
+				IPA.VOWEL_NEAROPEN_FRONT_UNROUNDED,
 				null
 			],
 			[null, null],
 			[
-				[sound("ɐ"), "ipa_vowel_nearopen_central"],
+				IPA.VOWEL_NEAROPEN_CENTRAL,
 				null
 			],
 			[null, null],
@@ -94,18 +94,18 @@ func _ready() -> void:
 		],
 		[
 			[
-				[sound("a"), "ipa_vowel_open_front_unrounded"],
-				[sound("ɶ"), "ipa_vowel_open_front_rounded"],
+				IPA.VOWEL_OPEN_FRONT_UNROUNDED,
+				IPA.VOWEL_OPEN_FRONT_ROUNDED,
 			],
 			[null, null],
 			[
-				[sound("ä"), "ipa_vowel_open_central"],
+				IPA.VOWEL_OPEN_CENTRAL,
 				null
 			],
 			[null, null],
 			[
-				[sound("ɑ"), "ipa_vowel_open_back_unrounded"],
-				[sound("ɒ"), "ipa_vowel_open_back_rounded"]
+				IPA.VOWEL_OPEN_BACK_UNROUNDED,
+				IPA.VOWEL_OPEN_BACK_ROUNDED
 			],
 		],
 	]
