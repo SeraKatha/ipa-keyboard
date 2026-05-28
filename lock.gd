a9e67c14304e1ee48b2f005108d85889d3f813bc
Sera Katharina Litsch <sera.katharina.litsch@gmail.com>
2026-05-28 17:58:53 +0200

    Fixed locking and cleaned up config loading

--- lock.gd
+++ lock.gd
@@ -2,34 +2,46 @@ extends Node
 
 const LOCK_FILE_PATH : String = "user://lock"
 const LOCK_EXPIRATION = 2;
+var _locked = false;
 
 
 func _is_lock_fresh():
 	var lock_file = FileAccess.open(LOCK_FILE_PATH, FileAccess.READ)
 	var timestamp_lock = lock_file.get_64()
 	lock_file.close()
-	var timestamp_now = Time.get_unix_time_from_system()
+	var timestamp_now = Time.get_unix_time_from_system() as int
 	var is_already_running = timestamp_now - timestamp_lock < LOCK_EXPIRATION
 	return is_already_running
 
 
+func _acquire_lock() -> bool:
+	var already_locked = FileAccess.file_exists(LOCK_FILE_PATH) and _is_lock_fresh()
+	_locked = not(already_locked)
+	return _locked
+
+
 func try_lock() -> bool:
-	if FileAccess.file_exists(LOCK_FILE_PATH) and _is_lock_fresh():
-		get_tree().quit()
-		return false
-	else:
+	if _acquire_lock():
 		_refresh_lock()
-		return true
+	else:
+		get_tree().quit()
+	return _locked
 
 
 func _refresh_lock():
 	var	lock_file = FileAccess.open(LOCK_FILE_PATH, FileAccess.WRITE)
-	lock_file.store_64(Time.get_unix_time_from_system())
+	var timestamp = Time.get_unix_time_from_system() as int
+	lock_file.store_64(timestamp)
 	lock_file.close()
 
 
+func _remove_lock():
+	if _locked:
+		DirAccess.remove_absolute(LOCK_FILE_PATH)
+
+
 func _exit_tree() -> void:
-	DirAccess.remove_absolute(LOCK_FILE_PATH)
+	_remove_lock()
 
 
 func _on_lock_timer_timeout() -> void:
--- main.gd
+++ main.gd
@@ -27,30 +27,43 @@ func _init_config():
 		config.save("user://config.cfg")
 
 
-func _load_config():
-	var config = ConfigFile.new()
-	config.load("user://config.cfg")
-	var application_scale = config.get_value("ui", "scale", 1.0)
+func _align_window():
+	var window_size : Vector2 = DisplayServer.window_get_size();
+	var screen_size : Vector2 = DisplayServer.screen_get_size();
+	var window_position : Vector2 = screen_size / Vector2(2.0, 1.0) - window_size / Vector2(2, 1)
+	DisplayServer.window_set_position(Vector2i(round(window_position)))
+
+
+func _load_config_ui(config_file : ConfigFile) -> void:
+	var application_scale = config_file.get_value("ui", "scale", 1.0)
 	get_tree().root.content_scale_factor = application_scale
 	var window_size : Vector2 = Vector2(890, 344) * application_scale
-	DisplayServer.window_set_size(window_size)
-	if config.has_section("key_bindings"):
-		for key in config.get_section_keys("key_bindings"):
-			var event_action_str = config.get_value("key_bindings", key).to_lower()
+	DisplayServer.window_set_size(Vector2i(window_size))
+
+
+func _load_config_key_bindings(config_file : ConfigFile) -> void:
+	if config_file.has_section("key_bindings"):
+		for key in config_file.get_section_keys("key_bindings"):
+			var event_action_str = config_file.get_value("key_bindings", key).to_lower()
 			var event_action_keys = event_action_str.split("+")
 			var input_event_key = InputMap.action_get_events(key)[0]
 			input_event_key.shift_pressed = event_action_keys.has("shift")
 			input_event_key.ctrl_pressed = event_action_keys.has("ctrl")
 			input_event_key.alt_pressed = event_action_keys.has("alt")
 			input_event_key.keycode = OS.find_keycode_from_string(event_action_keys[-1])
-	var screen_size : Vector2 = DisplayServer.screen_get_size();
-	var window_position : Vector2 = screen_size / Vector2(2.0, 1.0) - window_size / Vector2(2, 1)
-	DisplayServer.window_set_position(Vector2i(round(window_position)))
+
+
+func _load_config():
+	var config_file = ConfigFile.new()
+	config_file.load("user://config.cfg")
+	_load_config_ui(config_file)
+	_load_config_key_bindings(config_file)
 
 
 func _ready() -> void:
 	_init_config()
 	_load_config()
+	_align_window()
 	if _lock.try_lock():
 		_keyboard = KEYBOARD.instantiate()
 		_container.add_child(_keyboard)
